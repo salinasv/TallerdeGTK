dd3f1c3fbc249e7f2a85dcdde532cfc36e8b5306
salinasv <salinasv@gmail.com>
2009-09-15 17:17:09 -0500

    Agregar bosquejo de menú y toolbar a la ventana.

--- gtk/main.c
+++ gtk/main.c
@@ -1,5 +1,42 @@
 #include <gtk/gtk.h>
  
+static void lookup_character_action()
+{
+	g_print("lookup\n");
+}
+
+static void clear_character_action()
+{
+	g_print("clear!\n");
+}
+
+static void quit_action()
+{
+	gtk_main_quit();
+}
+
+static GtkActionEntry entries[] = 
+{
+  { "FileMenuAction", NULL, "_File" },                  /* name, stock id, label */
+  { "CharacterMenuAction", NULL, "_Character" },
+
+  
+  { "LookupAction", GTK_STOCK_FIND,                             /* name, stock id */
+    "_Lookup", "<control>L",                                    /* label, accelerator */
+    "Look-up the character drawn",                              /* tooltip */ 
+    G_CALLBACK (lookup_character_action) },
+    
+  { "ClearAction", GTK_STOCK_OPEN,
+    "_Clear","<control>C",  
+    "Clear the drawing area",
+    G_CALLBACK (clear_character_action) },
+    
+  { "QuitAction", GTK_STOCK_QUIT,
+    "_Quit", "<control>Q",    
+    "Quit",
+    G_CALLBACK (quit_action) }
+};
+static guint n_entries = G_N_ELEMENTS (entries);
 
 void 
 on_window1_destroy (GtkObject *object, gpointer user_data)
@@ -15,6 +52,13 @@ int
 main (int argc, char *argv[])
 {
         GtkWidget	*window1, *button, *frame, *label;
+		GtkWidget *menu_box;
+		GtkWidget *menubar;
+		GtkWidget *toolbar;
+		GtkUIManager *ui_manager;
+		GtkActionGroup *action_group;
+		GError *error = NULL;
+		gint ui_id;
 
         gtk_init (&argc, &argv);
         
@@ -22,9 +66,27 @@ main (int argc, char *argv[])
 		g_signal_connect (G_OBJECT (window1), "destroy",G_CALLBACK (on_window1_destroy), NULL);
 		gtk_window_set_title(GTK_WINDOW(window1), "Bla bla bla");
 		
+		/* GtkUIManager Menu and tooltip stuff */
+		action_group = gtk_action_group_new("TestActions");
+		gtk_action_group_add_actions(action_group, entries, n_entries, NULL);
+
+		ui_manager = gtk_ui_manager_new();
+		gtk_ui_manager_insert_action_group(ui_manager, action_group, 0);
+
+		ui_id = gtk_ui_manager_add_ui_from_file(ui_manager, "menus.xml", &error);
+
+		/* Pack the menu and toolbar */
+		menu_box = gtk_vbox_new(FALSE, 0);
+		gtk_container_add(GTK_CONTAINER(window1), menu_box);
+
+		menubar = gtk_ui_manager_get_widget(ui_manager, "/MainMenu");
+		toolbar = gtk_ui_manager_get_widget(ui_manager, "/MainToolbar");
+		gtk_box_pack_start(GTK_BOX(menu_box), menubar, FALSE, FALSE, 0);
+		gtk_box_pack_start(GTK_BOX(menu_box), toolbar, FALSE, FALSE, 0);
+
+		/* Frame stuff */
 		frame = gtk_fixed_new();
 		gtk_container_add(GTK_CONTAINER(window1),frame);
-
 
 		label = gtk_label_new("");
 		button = gtk_button_new_with_label("button");
