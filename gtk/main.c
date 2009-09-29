62f663f882f4dfc2fc49c264c8275a9153863bb1
salinasv <salinasv@gmail.com>
2009-09-29 01:08:41 -0500

    Agregr label y entry para leer el nombre.

--- gtk/main.c
+++ gtk/main.c
@@ -1,5 +1,14 @@
 #include <gtk/gtk.h>
 
+typedef struct name_
+{
+	GtkWidget *name;
+	GtkWidget *pat;
+	GtkWidget *mat;
+	GtkWidget *label;
+	gchar *fullname;
+}Name;
+
 static void lookup_character_action()
 {
 	g_print("lookup\n");
@@ -48,20 +57,38 @@ on_window1_destroy (GtkObject *object, gpointer user_data)
 	gtk_main_quit();
 }
 
-void on_button_pressed (GtkButton *button, gpointer label){
-	gtk_label_set_text(GTK_LABEL(label),"hola");
+void on_button_pressed (GtkButton *button, gpointer userdata){
+	Name *usrname = (Name*)userdata;
+
+	g_free(usrname->fullname);
+
+	usrname->fullname = NULL;
+	usrname->fullname = g_strconcat(
+			"Tu nombre es: ",
+			gtk_entry_get_text(GTK_ENTRY(usrname->name)),
+			" ",
+			gtk_entry_get_text(GTK_ENTRY(usrname->pat)),
+			" ",
+			gtk_entry_get_text(GTK_ENTRY(usrname->mat)),
+			NULL);
+	gtk_label_set_text(GTK_LABEL(usrname->label), usrname->fullname);
 }
 
 	int
 main (int argc, char *argv[])
 {
-	GtkWidget	*window1, *button, *frame, *label;
+	GtkWidget	*window1, *button;
+	GtkWidget *main_win;
+	GtkWidget *label;
+	GtkWidget *hbox;
+	GtkWidget *vbox;
 	GtkWidget *menu_box;
 	GtkWidget *menubar;
 	GtkWidget *toolbar;
 	GtkUIManager *ui_manager;
 	GtkActionGroup *action_group;
 	GError *error = NULL;
+	Name *usr_name;
 	gint ui_id;
 
 	gtk_init (&argc, &argv);
@@ -88,21 +115,48 @@ main (int argc, char *argv[])
 	gtk_box_pack_start(GTK_BOX(menu_box), menubar, FALSE, FALSE, 0);
 	gtk_box_pack_start(GTK_BOX(menu_box), toolbar, FALSE, FALSE, 0);
 
-	/* Frame stuff */
-	frame = gtk_fixed_new();
-	gtk_container_add(GTK_CONTAINER(window1),frame);
+	/* Main window stuff */
+	main_win = gtk_vbox_new(FALSE, 0);
+	gtk_box_pack_start(GTK_BOX(menu_box),main_win, FALSE, FALSE, 0);
 
-	label = gtk_label_new("");
+	usr_name = g_new(Name, 1);
+	usr_name->label = gtk_label_new("Tu nombre es:");
+
+	hbox = gtk_hbox_new(FALSE, 0);
+
+	/* Labels */
+	vbox = gtk_vbox_new(TRUE, 0);
+	label = gtk_label_new("Nombre");
+	gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
+	label = gtk_label_new("Apellido Paterno");
+	gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
+	label = gtk_label_new("Apellido Materno");
+	gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
+
+	gtk_box_pack_start(GTK_BOX(hbox), vbox, FALSE, FALSE, 0);
+
+	/* Entries */
+	vbox = gtk_vbox_new(FALSE, 0);
+	usr_name->name = gtk_entry_new();
+	gtk_box_pack_start(GTK_BOX(vbox), usr_name->name, FALSE, FALSE, 0);
+	usr_name->pat = gtk_entry_new();
+	gtk_box_pack_start(GTK_BOX(vbox), usr_name->pat, FALSE, FALSE, 0);
+	usr_name->mat = gtk_entry_new();
+	gtk_box_pack_start(GTK_BOX(vbox), usr_name->mat, FALSE, FALSE, 0);
+
+	gtk_box_pack_start(GTK_BOX(hbox), vbox, FALSE, FALSE, 0);
+
+	gtk_box_pack_start(GTK_BOX(main_win), hbox, FALSE, FALSE, 0);
+
 	button = gtk_button_new_with_label("button");
-	g_signal_connect(G_OBJECT(button), "pressed", G_CALLBACK(on_button_pressed), label);
-	gtk_fixed_put(GTK_FIXED(frame),button,40,10);
+	g_signal_connect(G_OBJECT(button), "pressed", G_CALLBACK(on_button_pressed), usr_name);
+	gtk_box_pack_start(GTK_BOX(main_win),button, FALSE, FALSE, 0);
 
-	gtk_fixed_put(GTK_FIXED(frame),label,10,0);
+	gtk_box_pack_start(GTK_BOX(main_win), usr_name->label, FALSE, FALSE, 0);
 	//gtk_container_add(GTK_CONTAINER(window1),button);
 	//gtk_container_add(GTK_CONTAINER(window1),label);
 
 	gtk_widget_show_all(window1);
-	gtk_label_set_text(GTK_LABEL(label),"");
 	gtk_main ();
 
 	return 0;
